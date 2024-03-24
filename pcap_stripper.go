72cabe3d40e1d805307f2b5cb82f514ab00f1f54
nikolay <svde@outlook.com>
2024-03-24 19:22:01 +0000

    Update pcap_stripper.go

--- pcap_stripper.go
+++ pcap_stripper.go
@@ -4,16 +4,206 @@ import (
     "fmt"
     "log"
     "os"
+    "strings"
 
     "github.com/google/gopacket"
     "github.com/google/gopacket/layers"
     "github.com/google/gopacket/pcapgo"
 )
 
+func stripBytesFromBeginning(inputFile, outputFile string, numBytes int) error {
+    // Open the input PCAP file
+    input, err := os.Open(inputFile)
+    if err != nil {
+        return err
+    }
+    defer input.Close()
 
+    // Create a new PCAP file reader
+    reader, err := pcapgo.NewReader(input)
+    if err != nil {
+        return err
+    }
+
+    // Create the output PCAP file
+    output, err := os.Create(outputFile)
+    if err != nil {
+        return err
+    }
+    defer output.Close()
+
+    // Create a new PCAP file writer
+    writer := pcapgo.NewWriter(output)
+    writer.WriteFileHeader(1024, layers.LinkTypeEthernet)
+
+    // Iterate over each packet in the input PCAP file
+    for {
+        data, ci, err := reader.ReadPacketData()
+        if err != nil {
+            if err.Error() == "EOF" {
+                break
+            }
+            return err
+        }
+
+        // Strip the specified number of bytes from the beginning of each packet
+        strippedPacket := data[numBytes:]
+
+        // Write the stripped packet to the output PCAP file
+        writer.WritePacket(ci, strippedPacket)
+    }
+
+    fmt.Printf("Stripped %d bytes from the beginning of each packet and saved to %s\n", numBytes, outputFile)
+    return nil
+}
+
+func stripBytesFromEnd(inputFile, outputFile string, numBytes int) error {
+    // Open the input PCAP file
+    input, err := os.Open(inputFile)
+    if err != nil {
+        return err
+    }
+    defer input.Close()
+
+    // Create a new PCAP file reader
+    reader, err := pcapgo.NewReader(input)
+    if err != nil {
+        return err
+    }
+
+    // Create the output PCAP file
+    output, err := os.Create(outputFile)
+    if err != nil {
+        return err
+    }
+    defer output.Close()
+
+    // Create a new PCAP file writer
+    writer := pcapgo.NewWriter(output)
+    writer.WriteFileHeader(1024, layers.LinkTypeEthernet)
+
+    // Iterate over each packet in the input PCAP file
+    for {
+        data, ci, err := reader.ReadPacketData()
+        if err != nil {
+            if err.Error() == "EOF" {
+                break
+            }
+            return err
+        }
+
+        // Strip the specified number of bytes from the end of each packet
+        strippedPacket := data[:len(data)-numBytes]
+
+        // Write the stripped packet to the output PCAP file
+        writer.WritePacket(ci, strippedPacket)
+    }
+
+    fmt.Printf("Stripped %d bytes from the end of each packet and saved to %s\n", numBytes, outputFile)
+    return nil
+}
+
+func stripUntilSecondEthernet(inputFile, outputFile string) error {
+    // Open the input PCAP file
+    input, err := os.Open(inputFile)
+    if err != nil {
+        return err
+    }
+    defer input.Close()
+
+    // Create a new PCAP file reader
+    reader, err := pcapgo.NewReader(input)
+    if err != nil {
+        return err
+    }
+
+    // Create the output PCAP file
+    output, err := os.Create(outputFile)
+    if err != nil {
+        return err
+    }
+    defer output.Close()
+
+    // Create a new PCAP file writer
+    writer := pcapgo.NewWriter(output)
+    writer.WriteFileHeader(1024, layers.LinkTypeEthernet)
+
+    // Iterate over each packet in the input PCAP file
+    for {
+        data, ci, err := reader.ReadPacketData()
+        if err != nil {
+            if err.Error() == "EOF" {
+                break
+            }
+            return err
+        }
+
+        packet := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default)
+        ethernetLayers := packet.LayersByType(layers.LayerTypeEthernet)
+
+        if len(ethernetLayers) >= 2 {
+            // Find the offset of the second Ethernet header
+            secondEthernetOffset := ethernetLayers[1].(*layers.Ethernet).Contents[0] & 0xf
+
+            // Strip the outer headers before the second Ethernet header
+            strippedPacket := data[secondEthernetOffset:]
+            writer.WritePacket(ci, strippedPacket)
+        } else {
+            writer.WritePacket(ci, data)
+        }
+    }
+
+    fmt.Printf("Stripped outer headers until the second Ethernet header for all packets and saved to %s\n", outputFile)
+    return nil
+}
 
 func filterPackets(inputFile, outputFile, displayFilter string) error {
-    // ... (code remains unchanged)
+    // Open the input PCAP file
+    input, err := os.Open(inputFile)
+    if err != nil {
+        return err
+    }
+    defer input.Close()
+
+    // Create a new PCAP file reader
+    reader, err := pcapgo.NewReader(input)
+    if err != nil {
+        return err
+    }
+
+    // Create the output PCAP file
+    output, err := os.Create(outputFile)
+    if err != nil {
+        return err
+    }
+    defer output.Close()
+
+    // Create a new PCAP file writer
+    writer := pcapgo.NewWriter(output)
+    writer.WriteFileHeader(1024, layers.LinkTypeEthernet)
+
+    // Iterate over each packet in the input PCAP file
+    for {
+        data, ci, err := reader.ReadPacketData()
+        if err != nil {
+            if err.Error() == "EOF" {
+                break
+            }
+            return err
+        }
+
+        // Create a packet from the raw data
+        packet := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default)
+
+        // Check if the packet matches the display filter
+        if matchFilter(packet, displayFilter) {
+            // Write the matched packet to the output PCAP file
+            writer.WritePacket(ci, data)
+        }
+    }
+
+    fmt.Printf("Filtered packets based on the display filter '%s' and saved to %s\n", displayFilter, outputFile)
+    return nil
 }
 
 func matchFilter(packet gopacket.Packet, filter string) bool {
